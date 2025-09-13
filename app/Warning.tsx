017abebed59f26ccc75a2b017a24d2e2d2e0c377
Alexander Prechtel <aprechtel0043@gmail.com>
2025-09-12 23:59:33 -0400

    init

---
+++ app/Warning.tsx
@@ -0,0 +1,11 @@
+export default function Warning(){
+    return <div style={{
+        border: '1px solid red',
+        padding: '16px',
+        borderRadius: '8px',
+        backgroundColor: '#f9f9f9',
+        marginBottom: '16px'
+    }}>
+        HELLO WORLD
+    </div>
+}
