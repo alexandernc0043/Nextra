363cc4723d17f58d7bcbc924fb423d0a5f004a23
Alexander Prechtel <aprechtel0043@gmail.com>
2025-10-24 19:47:35 -0400

    working

---
+++ app/_meta.global.ts
@@ -0,0 +1,71 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  index: {
+    title: "Home",
+    type: "doc",
+    theme: {
+      toc: false,
+    },
+  },
+  modules: {
+    items: {
+      one: {
+        title: "Module 1",
+        type: "page",
+      },
+      two: {
+        title: "Module 1",
+        type: "page",
+      },
+      three: {
+        title: "Module 1",
+        type: "page",
+      },
+      four: {
+        title: "Module 1",
+        type: "page",
+      },
+      five: {
+        title: "Module 1",
+        type: "page",
+      },
+      six: {
+        title: "Module 1",
+        type: "page",
+      },
+    },
+  },
+  // modules: {
+  //   type: "menu",
+  //   title: "Modules",
+  //   items: {
+  //     one: {
+  //       title: "Module 1",
+  //       type: "page",
+  //     },
+  //     two: {
+  //       title: "Module 2",
+  //       type: "page",
+  //     },
+  //     three: {
+  //       title: "Module 3",
+  //       type: "page",
+  //     },
+  //     four: {
+  //       title: "Module 4",
+  //       type: "page",
+  //     },
+  //     five: {
+  //       title: "Module 5",
+  //       type: "page",
+  //     },
+  //     six: {
+  //       title: "Module 6",
+  //       type: "page",
+  //     },
+  //   },
+  // },
+};
+
+export default meta;
